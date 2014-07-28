45a480755a904a2a22d438f18fc6d0a5d0664b90
Sagar Bhandari <webgig.sagar@gmail.com>
2014-07-28 14:46:57 +1000

    Added beforeLiveLogin event. Some params like redirect uri can be overriden using this event

--- src/ScnSocialAuth/HybridAuth/Provider/Live.php
+++ src/ScnSocialAuth/HybridAuth/Provider/Live.php
@@ -1,11 +1,15 @@
 <?php
 namespace ScnSocialAuth\HybridAuth\Provider;
 
+use Zend\EventManager\EventManagerInterface;
+use Zend\EventManager\EventManager;
+use Zend\EventManager\EventManagerAwareInterface;
+
 /**
  * This is simply to trigger autoloading as a hack for poor design in HybridAuth.
  */
 
-class Live extends \Hybrid_Providers_Live
+class Live extends \Hybrid_Providers_Live  implements EventManagerAwareInterface
 {
     //Override the default login behavior of the hybridauth which passes the redirect_uri as querystring  and
     //creates possible mismatch with the registerd redirect_url in the providers api settings
@@ -24,19 +28,39 @@ class Live extends \Hybrid_Providers_Live
             $HYBRID_AUTH_URL_BASE = \Hybrid_Auth::$config["base_url"];
         }
 
+
         $params["scope"]         = $this->scope;
-        $params["redirect_uri"]  = $HYBRID_AUTH_URL_BASE . ( strpos( $HYBRID_AUTH_URL_BASE, '?' ) ? '&' : '/' ) . "done/live";
 
-           $this->api->redirect_uri = $params["redirect_uri"] ;
+        $this->api->redirect_uri = $HYBRID_AUTH_URL_BASE . ( strpos( $HYBRID_AUTH_URL_BASE, '?' ) ? '&' : '/' ) . "done/live" ;
 
-        \Hybrid_Auth::storage()->set( "hauth_session.live.hauth_endpoint" , $params["redirect_uri"] );
+        \Hybrid_Auth::storage()->set( "hauth_session.live.hauth_endpoint" , $this->api->redirect_uri  );
 
         $provider_params = \Hybrid_Auth::storage()->get( "hauth_session.live.id_provider_params");
-        $provider_params['login_done'] = $params["redirect_uri"];
+        $provider_params['login_done'] = $this->api->redirect_uri ;
 
         \Hybrid_Auth::storage()->set( "hauth_session.live.id_provider_params" , $provider_params );
 
+        $this->getEventManager()->trigger('beforeLiveLogin', $this, array('api' => $this->api,'params' => &$params, 'provider_params' => $provider_params));
+
         // redirect the user to the provider authentication url
-        \Hybrid_Auth::redirect( $this->api->authorizeUrl( $params) );
+        \Hybrid_Auth::redirect( $this->api->authorizeUrl($params ) );
+    }
+
+    public function setEventManager(EventManagerInterface $events)
+    {
+        $events->setIdentifiers(array(
+            __CLASS__,
+            get_called_class(),
+        ));
+        $this->events = $events;
+        return $this;
+    }
+
+    public function getEventManager()
+    {
+        if (null === $this->events) {
+            $this->setEventManager(new EventManager());
+        }
+        return $this->events;
     }
 }
